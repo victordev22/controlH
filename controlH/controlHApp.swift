a143171a226357e0de6a4c359fbf73f022f0a65f
victordev <victor.dev2290>
2026-05-19 16:37:03 +0200

    Initial Commit

---
+++ controlH/controlHApp.swift
@@ -0,0 +1,17 @@
+//
+//  controlHApp.swift
+//  controlH
+//
+//  Created by user297436 on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct controlHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
